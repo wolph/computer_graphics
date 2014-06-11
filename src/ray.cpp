54a820d086f10c3ce5b43ffe674270e602377aee
Rick van Hattem <Wolph@wol.ph>
2014-06-11 10:47:19 +0200

    added intersect method for the ray

--- src/ray.cpp
+++ src/ray.cpp
@@ -13,3 +13,31 @@ Ray::Ray(){
 Ray::~Ray(){
 }
 
+bool Ray::intersect(Vec3Df triangle){
+//     T tmin = (min.x - r.orig.x) / r.dir.x;
+//     T tmax = (max.x - r.orig.x) / r.dir.x;
+//     if (tmin > tmax) swap(tmin, tmax);
+//     T tymin = (min.y - r.orig.y) / r.dir.y;
+//     T tymax = (max.y - r.orig.y) / r.dir.y;
+//     if (tymin > tymax) swap(tymin, tymax);
+//     if ((tmin > tymax) || (tymin > tmax))
+//         return false;
+//     if (tymin > tmin)
+//         tmin = tymin;
+//     if (tymax < tmax)
+//         tmax = tymax;
+//     T tzmin = (min.z - r.orig.z) / r.dir.z;
+//     T tzmax = (max.z - r.orig.z) / r.dir.z;
+//     if (tzmin > tzmax) swap(tzmin, tzmax);
+//     if ((tmin > tzmax) || (tzmin > tmax))
+//         return false;
+//     if (tzmin > tmin)
+//         tmin = tzmin;
+//     if (tzmax < tmax)
+//         tmax = tzmax;
+//     if ((tmin > r.tmax) || (tmax < r.tmin)) return false;
+//     if (r.tmin < tmin) r.tmin = tmin;
+//     if (r.tmax > tmax) r.tmax = tmax;
+     return true;
+}
+
--- src/ray.h
+++ src/ray.h
@@ -8,10 +8,13 @@
 #ifndef RAY_H_
 #define RAY_H_
 
+#include "mesh.h"
+
 class Ray{
 public:
     Ray();
     virtual ~Ray();
+    bool intersect(Vec3Df triangle);
 };
 
 #endif /* RAY_H_ */
