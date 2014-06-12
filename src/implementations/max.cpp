043ef9f71ba839aa51600956d47d0df006d8ca5c
MaxGroenenboom <Max_Groenenboom@hotmail.com>
2014-06-12 20:40:26 +0200

    Made intersect function
    Created an intersect function and added some of the raytracing code to
    it.

--- src/implementations/max.cpp
+++ src/implementations/max.cpp
@@ -3,20 +3,16 @@
 #include "../Mesh.h"
 
 Ray performRayTracingMax(Ray ray){
-	std::vector<Vertex> vertices = MyMesh.vertices;
+	
 	std::vector<Triangle> triangles = MyMesh.triangles;
 	ray.setColor(Vec3Df(0, 0, 0));
-	float resColor = 1000.0f;
+	//float resColor = 1000.0f;
+	bool hit = false;
 	for (unsigned int i = 0; i < triangles.size(); i++){
-		Triangle curT = triangles[i];
-		Vec3Df triangleNormal = crossProduct(minus_(vertices[curT.v[1]].p, vertices[curT.v[0]].p), minus_(vertices[curT.v[2]].p, vertices[curT.v[0]].p));
-		float planeD = dotProduct(triangleNormal, vertices[curT.v[0]].p);
-		float rayD = -(dotProduct(triangleNormal, ray.getOrig()) + planeD ) / dotProduct(triangleNormal, ray.getDest());
-		if (rayD < resColor)
-			resColor = rayD;
+		hit |= intersect(triangles[i], ray);
 	}
-	ray.setColor(Vec3Df(resColor/10, resColor, resColor/100));
-	std::cout << resColor << std::endl;
+	if (hit)
+		ray.setColor(Vec3Df(0, 1, 0));
 	return ray;
 }
 
@@ -32,4 +28,22 @@ float dotProduct(Vec3Df in1, Vec3Df in2){
 
 inline Vec3Df minus_(Vec3Df in1, Vec3Df in2){
 	return Vec3Df(in1[0] - in2[0], in1[1] - in2[1], in1[2] - in2[2]);
+}
+
+bool intersect(Triangle t, Ray ray){
+	std::vector<Vertex> vertices = MyMesh.vertices;
+	Vec3Df triangleNormal = crossProduct(minus_(vertices[t.v[1]].p, vertices[t.v[0]].p), minus_(vertices[t.v[2]].p, vertices[t.v[0]].p));
+	if (dotProduct(triangleNormal, ray.getDest()) == 0)
+		return false;
+	float origD = dotProduct(triangleNormal, vertices[t.v[0]].p);
+	float rayD = -(dotProduct(triangleNormal, ray.getOrig()) + origD) / dotProduct(triangleNormal, ray.getDest());
+	Vec3Df rayHit = ray.getOrig() + rayD * ray.getDest();
+
+	if (dotProduct(triangleNormal, crossProduct(minus_(vertices[t.v[1]].p, vertices[t.v[0]].p), minus_(rayHit, vertices[t.v[0]].p))) < 0)
+		return false;
+	if (dotProduct(triangleNormal, crossProduct(minus_(vertices[t.v[2]].p, vertices[t.v[1]].p), minus_(rayHit, vertices[t.v[1]].p))) < 0)
+		return false;
+	if (dotProduct(triangleNormal, crossProduct(minus_(vertices[t.v[0]].p, vertices[t.v[2]].p), minus_(rayHit, vertices[t.v[2]].p))) < 0)
+		return false;
+	return true;
 }
