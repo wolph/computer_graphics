f16a86872ec3a36bcad526560b4e02a0bdc29aae
MaxGroenenboom <Max_Groenenboom@hotmail.com>
2014-06-15 19:48:58 +0200

    Raytracer now "properly" colors rays according to distance to object.

--- src/implementations/max.cpp
+++ src/implementations/max.cpp
@@ -2,6 +2,7 @@
 #include "../RayTracer.hpp"
 #include "../Mesh.hpp"
 #include "../Vec3D.hpp"
+#include <cmath>
 
 unsigned int raysTraced = 0;
 bool debug = false;
@@ -11,14 +12,13 @@ Ray performRayTracingMax(Ray ray){
 	
 	std::vector<Triangle> triangles = MyMesh.triangles;
 	ray.setColor(Vec3Df(0, 0, 0));
-	float hit = 0;
+	float hit = 10e6f;
 	for (unsigned int i = 0; i < triangles.size(); i++){
-		float newHit = intersect(triangles[i], ray);
-		if (newHit > hit)
-			hit = newHit;
+		hit = fmin(hit, intersect(triangles[i], ray));
 	}
-	//if (hit > 0)
-		ray.setColor(Vec3Df(hit, hit/10, hit/100));
+
+	if (hit < 10e6f)
+		ray.setColor(Vec3Df(hit/50, hit/50, hit/50));
 	return ray;
 }
 
@@ -34,7 +34,7 @@ float intersect(Triangle t, Ray ray){
 	float angle = dot(triangleNormal, rayDir); // The cosine of the angle of the vectors (dotproduct of the vectors).
 
 	if (angle == 0) // If the ray and the plane are parallel (so their angle is 0), they won't intersect.
-		return 0;
+		return 10e6f;
 
 	float rayD = -(dot(triangleNormal, rayOrig) + dot(triangleNormal, v0)) / angle; // The distance of the ray's origin
 		// to the plane in which lies the triangle.
@@ -42,11 +42,11 @@ float intersect(Triangle t, Ray ray){
 	Vec3Df rayHit = rayOrig + rayD * rayDir; // The intersection point of the ray and the plane in which lies the triangle.
 
 	if (dot(triangleNormal, cross(v1mv0, rayHit - v0)) < 0)
-		return 0;
+		return 10e6f;
 	if (dot(triangleNormal, cross(v2 - v1, rayHit - v1)) < 0)
-		return 0;
+		return 10e6f;
 	if (dot(triangleNormal, cross(v0 - v2, rayHit - v2)) < 0)
-		return 0;
+		return 10e6f;
 
 	return rayD;
 }
@@ -60,6 +60,6 @@ float dot(Vec3Df in1, Vec3Df in2){
 
 Vec3Df normal(Vec3Df in){
 	Vec3Df res = Vec3Df(in);
-	res /= res.normalize();
+	res.normalize();
 	return res;
 }
