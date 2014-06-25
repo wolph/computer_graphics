f145d3c06266478fe160737813a06cacb2f1e7c7
Rick van Hattem <Wolph@wol.ph>
2014-06-26 01:57:43 +0200

    no need for virtual destructors

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "Triangle.hpp"
+#include "constants.hpp"
 
 Triangle::Triangle(const Triangle & triangle) : material(triangle.material){
     vertices[0] = triangle.vertices[0];
@@ -21,40 +22,29 @@ Triangle::Triangle(const Triangle & triangle) : material(triangle.material){
 
 Triangle::Triangle(const Vertex & v0, const Vertex & v1, const Vertex & v2,
                    const Texture & t0, const Texture & t1, const Texture & t2, const Material & m) : material(m){
-    if(&v0 != 0)
-        vertices[0] = v0;
-    if(&v1 != 0)
-        vertices[1] = v1;
-    if(&v2 != 0)
-        vertices[2] = v2;
+    vertices[0] = v0;
+    vertices[1] = v1;
+    vertices[2] = v2;
 
-    if(&t0 != 0)
-        textures[0] = t0;
-    if(&t1 != 0)
-        textures[1] = t1;
-    if(&t2 != 0)
-		textures[2] = t2;
+    textures[0] = t0;
+    textures[1] = t1;
+    textures[2] = t2;
 
 	calculateNormal();
 }
 
 Triangle::Triangle(const Vertex & v0, const Vertex & v1, const Vertex & v2, const Material& m) : material(m){
-    if(&v0 != 0)
-        vertices[0] = v0;
-    if(&v1 != 0)
-        vertices[1] = v1;
-    if(&v2 != 0)
-		vertices[2] = v2;
+    vertices[0] = v0;
+    vertices[1] = v1;
+    vertices[2] = v2;
 
 	calculateNormal();
 }
 
-Triangle::~Triangle(){
-}
-
 
 void Triangle::calculateNormal(){
-	normal = vertices[0].position.crossProduct(vertices[1].position - vertices[0].position,
+	normal = vertices[0].position.crossProduct(
+        vertices[1].position - vertices[0].position,
 		vertices[2].position - vertices[0].position);
 	normal.normalize();
 }
